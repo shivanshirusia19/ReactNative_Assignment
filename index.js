96f1e9b5f2fbe21ca57a452cb24b74bebc0ec6f9
Shivanshi Rusia <shivanshi.rusia@gmail.com>
2021-03-05 13:22:11 +0530

    Updated the code structure

--- index.js
+++ index.js
@@ -5,12 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Task1 from './Task1';
-import Task2 from './Task2';
-import Task3 from './Task3';
+import Task1 from './src/components/Task1';
+import Task2 from './src/components/Task2';
+import Task3 from './src/components/Task3';
 
 //AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => App);
 //AppRegistry.registerComponent(appName, () => Task1);
 //AppRegistry.registerComponent(appName, () => Task2);
-//AppRegistry.registerComponent(appName, () => Task3);
+AppRegistry.registerComponent(appName, () => Task3);
