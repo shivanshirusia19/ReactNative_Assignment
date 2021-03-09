b0b2292ce1c343b84215974097bf755e5c24fa01
Shivanshi Rusia <shivanshi.rusia@gmail.com>
2021-03-09 15:51:36 +0530

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import Web_View from './src/components/Web_View';
+import SwitchSlider from './src/components/SwitchSlider';
+
+// AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => Web_View);
+AppRegistry.registerComponent(appName, () => SwitchSlider);
---
+++ src/components/Web_View.js
@@ -0,0 +1,57 @@
+// import React, {Component} from 'react';
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+//   SafeAreaView,
+//   ActivityIndicator,
+// } from 'react-native';
+// import {WebView} from 'react-native-webview';
+
+// // ...
+// export default class Web_View extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {isLoading: false};
+//   }
+//   render() {
+//     return (
+//       <SafeAreaView style={{flex: 1}}>
+//         <WebView
+//           onLoadStart={() => {
+//             this.setState({
+//               isLoading: true,
+//             });
+//           }}
+//           onLoadEnd={() => {
+//             this.setState({
+//               isLoading: false,
+//             });
+//           }}
+//           onNavigationStateChange={(a) => console.log(a)}
+//           source={{uri: 'https://google.com'}}
+//         />
+//         {this.state.isLoading ? (
+//           <View
+//             style={{
+//               flex: 1,
+//               position: 'absolute',
+//               justifyContent: 'center',
+//               alignItems: 'center',
+//               top: 0,
+//               left: 0,
+//               width: '100%',
+//               height: '100%',
+//             }}>
+//             <ActivityIndicator
+//               size={'large'}
+//               color={'gray'}
+//               animating={true}
+//               style={{alignSelf: 'center'}}
+//             />
+//           </View>
+//         ) : null}
+//       </SafeAreaView>
+//     );
+//   }
+// }
