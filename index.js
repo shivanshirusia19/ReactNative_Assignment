c7798a0d52a8c5d9370ca6630cc6ba52ad8d5054
Shivanshi Rusia <shivanshi.rusia@gmail.com>
2021-03-09 21:12:16 +0530

    index updated

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ import App from './App';
 import {name as appName} from './app.json';
 import Web_View from './src/components/Web_View';
 import SwitchSlider from './src/components/SwitchSlider';
+import FlatlistEx from './src/components/FlatlistEx';
 
 // AppRegistry.registerComponent(appName, () => App);
 //AppRegistry.registerComponent(appName, () => Web_View);
-AppRegistry.registerComponent(appName, () => SwitchSlider);
+// AppRegistry.registerComponent(appName, () => SwitchSlider);
+AppRegistry.registerComponent(appName, () => FlatlistEx);
