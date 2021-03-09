742226ed6d7a7ee14a40e47411e96bdeb04719fe
Shivanshi Rusia <shivanshi.rusia@gmail.com>
2021-03-09 15:54:59 +0530

    Implemented Web View

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ import Web_View from './src/components/Web_View';
 import SwitchSlider from './src/components/SwitchSlider';
 
 // AppRegistry.registerComponent(appName, () => App);
-// AppRegistry.registerComponent(appName, () => Web_View);
-AppRegistry.registerComponent(appName, () => SwitchSlider);
+AppRegistry.registerComponent(appName, () => Web_View);
+//AppRegistry.registerComponent(appName, () => SwitchSlider);
