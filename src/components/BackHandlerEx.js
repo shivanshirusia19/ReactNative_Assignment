3bdbac79f4cae15286b16e0f76e3c3b8a5d841ac
Shivanshi Rusia <shivanshi.rusia@gmail.com>
2021-03-11 22:57:59 +0530

    BackHandler Code Implemented

--- src/components/BackHandlerEx.js
+++ src/components/BackHandlerEx.js
@@ -1,54 +1,54 @@
-// import React from 'react';
-// import {View, StyleSheet, Text, BackHandler, Alert} from 'react-native';
+import React from 'react';
+import {View, StyleSheet, Text, BackHandler, Alert} from 'react-native';
 
-// class BackHandlerEx extends React.Component {
-//   componentDidMount() {
-//     this.backHandler = BackHandler.addEventListener(
-//       'hardwareBackPress',
-//       this.onBackPress,
-//     );
-//   }
+class BackHandlerEx extends React.Component {
+  componentDidMount() {
+    this.backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      this.onBackPress,
+    );
+  }
 
-//   componentWillUnmount() {
-//     this.backHandler.remove();
-//   }
+  componentWillUnmount() {
+    this.backHandler.remove();
+  }
 
-//   onBackPress = () => {
-//     Alert.alert('Exit App', 'Do you really want to exit?', [
-//       {
-//         text: 'Cancel',
-//         onPress: () => null,
-//         style: 'cancel',
-//       },
-//       {
-//         title: 'OK',
-//         onPress: () => {
-//           BackHandler.exitApp();
-//         },
-//       },
-//     ]);
-//     return 'true';
-//   };
+  onBackPress = () => {
+    Alert.alert('Exit App', 'Do you really want to exit?', [
+      {
+        text: 'Cancel',
+        onPress: () => null,
+        style: 'cancel',
+      },
+      {
+        title: 'OK',
+        onPress: () => {
+          BackHandler.exitApp();
+        },
+      },
+    ]);
+    return 'true';
+  };
 
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.text}>BackHandler Example</Text>
-//       </View>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>BackHandler Example</Text>
+      </View>
+    );
+  }
+}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   text: {
-//     fontSize: 20,
-//     fontWeight: '500',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+});
 
-// export default BackHandlerEx;
+export default BackHandlerEx;
