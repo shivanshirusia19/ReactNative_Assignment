77a213fa1b3768003d2230995db70ab7d8699536
Shivanshi Rusia <shivanshi.rusia@gmail.com>
2021-03-11 22:59:32 +0530

    Clipboard Code Implemented

--- src/components/DisplayClipboard.js
+++ src/components/DisplayClipboard.js
@@ -1,43 +1,43 @@
-// import React, {Component} from 'react';
-// import {View, Text, StyleSheet} from 'react-native';
-// import Clipboard from '@react-native-clipboard/clipboard';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
 
-// export default class DisplayClipboard extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       clipboardText: '',
-//     };
-//   }
+export default class DisplayClipboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clipboardText: '',
+    };
+  }
 
-//   componentDidMount() {
-//     (async () => {
-//       var textHolder = await Clipboard.getString();
-//       this.setState({
-//         clipboardText: textHolder,
-//       });
-//     })();
-//   }
+  componentDidMount() {
+    (async () => {
+      var textHolder = await Clipboard.getString();
+      this.setState({
+        clipboardText: textHolder,
+      });
+    })();
+  }
 
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.text}>Text copied from Clipboard is: </Text>
-//         <Text style={styles.text}>{this.state.clipboardText}</Text>
-//       </View>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Text copied from Clipboard is: </Text>
+        <Text style={styles.text}>{this.state.clipboardText}</Text>
+      </View>
+    );
+  }
+}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     margin: 20,
-//   },
-//   text: {
-//     fontSize: 18,
-//     margin: 20,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 20,
+  },
+  text: {
+    fontSize: 18,
+    margin: 20,
+  },
+});
--- src/components/InputClipboard.js
+++ src/components/InputClipboard.js
@@ -1,80 +1,68 @@
-// import React, {Component} from 'react';
-// import {
-//   View,
-//   StyleSheet,
-//   TextInput,
-//   TouchableOpacity,
-//   Button,
-//   Text,
-// } from 'react-native';
-// import Clipboard from '@react-native-clipboard/clipboard';
+import React, {Component} from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Button,
+  Text,
+} from 'react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
 
-// export default class InputClipboard extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       text: '',
-//     };
-//   }
+export default class InputClipboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+  }
 
-//   setTextIntoClipboard = async () => {
-//     await Clipboard.setString(this.state.text);
-//   };
+  setTextIntoClipboard = async () => {
+    await Clipboard.setString(this.state.text);
+  };
 
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.text}>Enter text and press long for copy</Text>
-//         <TouchableOpacity
-//           //   style={styles.touchable}
-//           onLongPress={() => this.setTextIntoClipboard()}>
-//           <TextInput
-//             style={styles.textInput}
-//             placeholder="Enter Text Here"
-//             onChangeText={(text) => {
-//               this.setState({text});
-//             }}
-//           />
-//         </TouchableOpacity>
-//         <Button
-//           //   color="#48d1cc"
-//           title="Copy TextInput Text Into Clipboard"
-//           onPress={() => this.props.navigation.navigate('Display')}
-//         />
-//       </View>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Enter text and press long for copy</Text>
+        <TouchableOpacity onLongPress={() => this.setTextIntoClipboard()}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Enter Text Here"
+            onChangeText={(text) => {
+              this.setState({text});
+            }}
+          />
+        </TouchableOpacity>
+        <Button
+          //   color="#48d1cc"
+          title="Copy TextInput Text Into Clipboard"
+          onPress={() => this.props.navigation.navigate('Display')}
+        />
+      </View>
+    );
+  }
+}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     margin: 20,
-//   },
-//   textInput: {
-//     textAlign: 'center',
-//     height: 40,
-//     width: 300,
-//     borderWidth: 1,
-//     borderColor: 'black',
-//     borderRadius: 8,
-//     marginBottom: 20,
-//     fontSize: 18,
-//   },
-//   text: {
-//     fontSize: 18,
-//     margin: 20,
-//   },
-//   //   touchable: {
-//   //     padding: 20,
-//   //     textAlign: 'center',
-//   //     height: 40,
-//   //     width: '90%',
-//   //     borderWidth: 1,
-//   //     borderColor: '#48d1cc',
-//   //     borderRadius: 8,
-//   //     marginBottom: 20,
-//   //   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 20,
+  },
+  textInput: {
+    textAlign: 'center',
+    height: 40,
+    width: 300,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 8,
+    marginBottom: 20,
+    fontSize: 18,
+  },
+  text: {
+    fontSize: 18,
+    margin: 20,
+  },
+});
