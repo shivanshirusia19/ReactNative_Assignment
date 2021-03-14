0cf68e886746031b5d71fdfa077ec1b886182eb5
Shivanshi Rusia <shivanshi.rusia@gmail.com>
2021-03-14 16:06:20 +0530

    Doubt Session Assignment

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
 import {AppRegistry} from 'react-native';
 //import App from './App';
 import {name as appName} from './app.json';
-//import SectionlistStack from './src/components/SectionlistStack';
-import Modalgif from './src/components/Modalgif';
+import SectionlistStack from './src/components/SectionlistStack';
+//import Modalgif from './src/components/Modalgif';
 
 // AppRegistry.registerComponent(appName, () => App);
-// AppRegistry.registerComponent(appName, () => SectionlistStack);
-AppRegistry.registerComponent(appName, () => Modalgif);
+AppRegistry.registerComponent(appName, () => SectionlistStack);
+//AppRegistry.registerComponent(appName, () => Modalgif);
