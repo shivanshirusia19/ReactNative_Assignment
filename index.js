f10f27c9fb6df1d8be1fc5aaa983234f4d03c5ea
Shivanshi Rusia <shivanshi.rusia@gmail.com>
2021-03-18 20:07:45 +0530

    assets updated

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
 import {AppRegistry} from 'react-native';
 //import App from './App';
 import {name as appName} from './app.json';
-//import MapsView2 from './src/components/MapsView2';
-import DraggableBall from './src/components/DraggableBall';
+import MapsView2 from './src/components/MapsView2';
+//import DraggableBall from './src/components/DraggableBall';
 
 // AppRegistry.registerComponent(appName, () => App);
-//AppRegistry.registerComponent(appName, () => MapsView2);
-AppRegistry.registerComponent(appName, () => DraggableBall);
+AppRegistry.registerComponent(appName, () => MapsView2);
+//AppRegistry.registerComponent(appName, () => DraggableBall);
